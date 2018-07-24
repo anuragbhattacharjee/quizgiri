a39121690aed264bb081774748eb5d804b96b52e
Anurag Bhattacharjee <anurag07061@gmail.com>
2018-07-24 20:39:57 +0600

    added cors support

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ var path = require('path');  // works with file system path
 var bodyParser = require('body-parser');
 var mongojs = require('mongojs');
 
+const app = express();
+//app.user(bodyParser.json());
+// after the code that uses bodyParser and other cool stuff
+var originsWhitelist = [
+    'http://localhost:4200',      //this is my front-end url for development
+     'https://quizgiri.herokuapp.com/'
+  ];
+  var corsOptions = {
+    origin: function(origin, callback){
+          var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+          callback(null, isWhitelisted);
+    },
+    credentials:true
+  }
+  //here is the magic
+  app.use(cors(corsOptions));
+
+
 const port = process.env.PORT || 5000;
 
-const app = express();
-app.use(cors());
+
+//app.use(cors());
 
 var index = require('./routes/index');
 
@@ -16,19 +34,23 @@ var db = mongojs("mongodb://anurag:anurag09@ds145921.mlab.com:45921/quizgiri", [
 //View Engine
 
 // path to the views : 
-app.set('views', path.join(__dirname,'quizgiri-clientapp/src'));
+app.set('views', path.join(__dirname,'views'));
 
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 //Set Static Folder to store the angular app
-app.use(express.static(path.join(__dirname, 'quizgiri-clientapp')));
+//app.use(express.static(path.join(__dirname, 'quizgiri-clientapp')));
 
 //Body Parser MiddleWare
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.use('/', index);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'quizgiri-clientapp/dist'));
+});
 
 //Get All Tasks
 app.get('/tasks', function(req, res, next){
